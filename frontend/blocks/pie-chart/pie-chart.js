1539f515ec0840efae6eb5b7d39153f739ef1f86
gulyaev52 <gulyaev52@gmail.com>
2017-01-19 19:42:32 +0300

    подправлены блоки. Прикручен js.

--- frontend/blocks/pie-chart/pie-chart.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-    $('.pie-chart').each(function() {
-        $(this).find(".pie-chart__items").peity('donut', { 
-            radius: 47.5,
-            innerRadius: 30.5
-        });
-    });
-});
--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-import './index.styl';
-
-// function requireAll(requireContext) {
-//     return requireContext.keys().map(requireContext);
-// }
-
-// requireAll(require.context('./plugins/', true, /^\.\/.*\.(js|css)$/));
-// requireAll(require.context('./blocks/', true, /^\.\/.*\.js$/));
