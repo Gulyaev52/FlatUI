f598dd7ff37e856e81e67ad33f665e6a010876b9
gulyaev52 <gulyaev52@gmail.com>
2017-01-21 05:24:06 +0300

    Убрано дублированние стилей для кнопок из блоков search / drop-down за счёт добавления блоку input элемента button

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,8 @@
 import './index.styl';
 
-function requireAll(requireContext) {
-    return requireContext.keys().map(requireContext);
-}
+// function requireAll(requireContext) {
+//     return requireContext.keys().map(requireContext);
+// }
 
-requireAll(require.context('./plugins/', true, /^\.\/.*\.(js|css)$/));
-requireAll(require.context('./blocks/', true, /^\.\/.*\.js$/));
+// requireAll(require.context('./plugins/', true, /^\.\/.*\.(js|css)$/));
+// requireAll(require.context('./blocks/', true, /^\.\/.*\.js$/));
